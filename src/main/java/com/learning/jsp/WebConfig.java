c0e3dec9794889d2c230057953967c163dbd43b1
Siwoo-Kim <skim327@myseneca.ca>
2018-01-26 14:29:12 -0500

    JSP_LEARNING 1.0

---
+++ src/main/java/com/learning/jsp/WebConfig.java
@@ -0,0 +1,11 @@
+package com.learning.jsp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.learning.jsp.controllers")
+public class WebConfig {
+
+
+}
